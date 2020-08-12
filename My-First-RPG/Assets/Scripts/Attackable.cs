55884c8dbdb6ce37020fad55b134199d4018ee78
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2020-08-12 14:54:54 +0200

    Attack push (basic) and health (basic)

--- My-First-RPG/Assets/Scripts/Attackable.cs
+++ My-First-RPG/Assets/Scripts/Attackable.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Health))]
 public class Attackable : MonoBehaviour
 {
+    private Health myHealth;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        myHealth = GetComponent<Health>(); //Each attackable object will have his own health (baseHealth and currentHealth)
+    }
+
     public void Attacked()
     {
-        Debug.Log("I have been attacked");
+        myHealth.CurrentHealth -= 1; //When attacked, the object will lose 1HP
+        Debug.Log("My Current Health is: " + myHealth.CurrentHealth.ToString());
+    }
+
+    public void Attacked(Vector2 attackDirection, int damage)
+    {
+        myHealth.ModifyHealth(-damage);
+        rb.AddForce(attackDirection * damage * 100);
     }
 }
---
+++ My-First-RPG/Assets/Scripts/Health.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int baseHealth;
+    private int currentHealth;
+    public int CurrentHealth
+    {
+        get
+        {
+            return currentHealth;
+        }
+        set
+        {
+            if (value > 0 && value <= baseHealth)
+            {
+                currentHealth = value;
+            }
+            else if(value > baseHealth)
+            {
+                currentHealth = baseHealth;
+            }
+            else
+            {
+                currentHealth = 0;
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    void Start()
+    {
+        currentHealth = baseHealth;
+    }
+
+    public void ModifyHealth(int quantity) //This metod will be used to take damage or to heal
+    {
+        CurrentHealth += quantity;
+    }
+}
