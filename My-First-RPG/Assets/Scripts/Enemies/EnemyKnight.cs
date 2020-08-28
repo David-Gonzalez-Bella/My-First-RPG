0e03673d41403c5c4bbdaf019086700bb67ae5b5
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2020-08-28 02:33:36 +0200

    Particles (no light) and enemy spawner
    -Fireball (no light yet)
    -Spawn and die animation for enemies
    -Sword flashes (no lights yet)
    -Enemy Spawner (optimization)

--- My-First-RPG/Assets/Scripts/Enemies/EnemyKnight.cs
+++ My-First-RPG/Assets/Scripts/Enemies/EnemyKnight.cs
@@ -8,9 +8,11 @@ using UnityEngine;
 
 public class EnemyKnight : Enemy //Enemy inherits from Monobehaviour. Therefore, so does EnemyKnight
 {
+    public GameObject swordFlash;
+
     public override void EnemyAttack() //The knight overrides the method to attack, as he attacks in his own way
     {
-        atk.PhysicalAttack(attackDirection, atrib.damage);
+        atk.PhysicalAttack(attackDirection, atrib.damage, swordFlash);
     }
     public override void FlipSprite() //Each enemy will flip his sprite in his own way, depending on the direction his sprite sheets look at
     {
