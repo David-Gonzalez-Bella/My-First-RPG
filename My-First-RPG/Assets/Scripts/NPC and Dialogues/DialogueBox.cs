909b17df24c237c1bfac4001b0e9e8f03aac9ca0
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2020-09-19 03:03:18 +0200

    Fixed Interactuable and dialogues almost finished

---
+++ My-First-RPG/Assets/Scripts/NPC and Dialogues/DialogueBox.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueBox : MonoBehaviour
+{
+    public static DialogueBox sharedInstance;
+    public TMP_Text speaker;
+    public TMP_Text content;
+    private Dialogue dialogue;
+    private int dialogueIndex = 0;
+
+    private void Awake()
+    {
+        if (sharedInstance == null)
+            sharedInstance = this;
+    }
+
+    public void StartDialogue(string speakerName, Dialogue npcDialogue)
+    {
+        gameObject.SetActive(true);
+        dialogue = npcDialogue;
+        speaker.text = speakerName;
+        content.text = dialogue.lines[dialogueIndex];
+    }
+
+    public void NextLine()
+    {
+        dialogueIndex++;
+        if (dialogueIndex >= dialogue.lines.Length)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        content.text = dialogue.lines[dialogueIndex]; 
+    }
+    
+}
